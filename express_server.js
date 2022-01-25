3436b42fd717f046f9264e55f8f9df4cbbc2fd2d
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-25 15:14:14 -0500

    Added a delete button and fixed a bug for my short urls.

--- express_server.js
+++ express_server.js
@@ -7,10 +7,12 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-const randomID = function generateRandomString() {
+function generateRandomString() {
   let newURL = Math.random().toString(36).substr(2,6);
   return newURL;
-}
+};
+
+const randomID = generateRandomString();
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -40,7 +42,7 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.redirect(`/urls/${randomID}`);         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${randomID}`);
   urlDatabase[randomID] = req.body.longURL;
 });
 
@@ -49,10 +51,13 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[randomID];
-  res.redirect(longURL);
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
-
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
