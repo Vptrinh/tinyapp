e672b221cbd3e23c42db99799d1100b2a0d363db
Vivian Trinh <vptrinh@ryerson.ca>
2022-01-25 21:26:40 -0500

    Added a logout button.

--- express_server.js
+++ express_server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-const PORT = 3000; // default port 8080
+const PORT = 2000; // default port 8080
 const path = require("path");
 
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 function generateRandomString() {
   let newURL = Math.random().toString(36).substr(2,6);
@@ -20,13 +23,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  }
+  res.render("urls_new", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -37,7 +44,7 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
@@ -48,7 +55,7 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -75,8 +82,12 @@ app.post("/urls/:shortURL/update", (req,res) => {
 });
 
 app.post("/login", (req, res) => {
-  console.log(req.body);
   const username = req.body.username
-  res.cookie('user', username);
-  res.redirect(`/urls`)
+  res.cookie('username', username);
+  res.redirect(`/urls`);
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect(`/urls`);
 });
